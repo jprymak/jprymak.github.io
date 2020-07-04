4383d9d6c308fb235ea111225bff5e8132a09baf
Jakub Prymak <jprymak@gmail.com>
2020-07-04 17:38:40 +0200

    style: change some styles

--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,35 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+fetch('https://api.github.com/users/jprymak/repos')
+.then(resp =>resp.json()) //odpowiedź zamieniamy na JSONa
+.then(resp =>{
+    for(let repo of resp){
+        const{name, description, html_url} = repo;
+        const projectsTemplate = document.querySelector(".projects__template--js");
+
+
+        const projectName = document.querySelector(".project-name");
+        const projectsDescription = document.querySelector(".project-description");
+        const projectsDemo = document.querySelector(".project-demo");
+        const projectsGithub = document.querySelector(".project-github");
+
+        
+
+
+        const projectCard = document.createElement("div");
+        projectCard.className = "project__info--js";
+
+        
+        
+        
+        // `<li>
+        // ${name} <a href = "${html_url}" title="link do repozytorium ${name}">link do githuba<a/>
+        // </li>
+        // `
+        projectsTemplate.innerHTML += projectCard;
+    }
+}) 
+.catch(error =>{
+    console.log(error)
+})
