2d7f866e430fc82bf4cd74a172a43f4b6d79495d
Jakub Prymak <jprymak@gmail.com>
2020-07-07 20:07:50 +0200

    refactor: projects section styles

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@ import "../scss/main.scss";
 
 /* place your code below */
 
-const projectsWrapper = document.querySelector(".projects__wrapper--js");
+const projectsWrapper = document.querySelector(".projects__container--js");
 
 fetch("https://api.github.com/users/jprymak/repos")
   .then((resp) => resp.json()) //odpowiedź zamieniamy na JSONa
@@ -19,8 +19,8 @@ fetch("https://api.github.com/users/jprymak/repos")
       projectTop.setAttribute("class", "project__top");
 
       ///////// Creating card content /////////////
-      const projectInfo = document.createElement("div");
-      projectInfo.setAttribute("class", "project__info");
+      const projectContent = document.createElement("div");
+      projectContent.setAttribute("class", "project__content");
 
       ///////// Creating content /////////////
 
@@ -28,25 +28,26 @@ fetch("https://api.github.com/users/jprymak/repos")
 
       if(homepage){
         content = `
-        <span class="grid-item-1">project:</span><p class="project-name">${name}</p>
-        <span class="grid-item-2">description:</span><p class="project-description">${description}</p>
-        <span class="grid-item-3">demo:</span><a href="${homepage}" target="blank" class="project-demo">&lt;<u class="link">see here</u>&gt;</a>
-        <span class="grid-item-4">github:</span><a href="${html_url}" target="blank" class="project-github">&lt;<u class="link">source code</u>&gt;</a>
+        <span class="project__label">project:</span><p class="project__name">${name}</p>
+        <span class="project__label">description:</span><p class="project__description">${description}</p>
+        <span class="project__label">demo:</span><a href="${homepage}" target="blank" class="project__demo">&lt;<u class="project__link">see here</u>&gt;</a>
+        <span class="project__label">github:</span><a href="${html_url}" target="blank" class="project__github">&lt;<u class="project__link">source code</u>&gt;</a>
         `;
         
       }else{
         content = `
-        <span class="grid-item-1">project:</span><p class="project-name">${name}</p>
-        <span class="grid-item-2">description:</span><p class="project-description">${description}</p>
-        <span class="grid-item-4">github:</span><a href="${html_url}" target="blank" class="project-github">&lt;<u class="link">source code</u>&gt;</a>
+        <span class="project__label">project:</span><p class="project__name">${name}</p>
+        <span class="project__label">description:</span><p class="project__description">${description}</p>
+        <span class="project__label">demo:</span><a href="${homepage}" target="blank" class="project__demo">&lt;<u class="project__link">see here</u>&gt;</a>
+        <span class="project__label">github:</span><a href="${html_url}" target="blank" class="project__github">&lt;<u class="project__link">source code</u>&gt;</a>
         `;
         
 
       }
    
       project.appendChild(projectTop);
-      project.appendChild(projectInfo);
-      projectInfo.innerHTML += content;
+      project.appendChild(projectContent);
+      projectContent.innerHTML += content;
 
       projectsWrapper.appendChild(project);
     }
