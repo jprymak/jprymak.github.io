2b2930c66a377b91d8ef0d76557e0f5afa487d45
Jakub Prymak <jprymak@gmail.com>
2021-05-09 14:25:51 +0200

    chore: sort repos by updated and descending when fetching, dont render portoflio

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,10 @@
 import "../scss/main.scss";
 
-/* place your code below */
-
 console.log('Hello! I\'m Jakub. I\'m really glad you are here')
 
 const projectsContainer = document.querySelector(".projects__container--js");
 
-fetch("https://api.github.com/users/jprymak/repos")
+fetch("https://api.github.com/users/jprymak/repos?sort=updated&direction=desc")
   .then((resp) => resp.json()) 
   .then((resp) => {
     for (let repo of resp) {
@@ -24,7 +22,7 @@ fetch("https://api.github.com/users/jprymak/repos")
         </div>
         `;
 
-      if (description && homepage) {
+      if (description && homepage && homepage!== 'https://jprymak.github.io/') {
         projectsContainer.innerHTML += content;
       }
     }
